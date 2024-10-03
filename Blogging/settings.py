b5aeb544ca1b080b562106811908883c731cee54
Divesh Kshirsagar <diveshkshirsagar42@gmail.com>
2024-10-03 07:57:59 -0700

    project complete only local computer image upload remaining

--- Blogging/settings.py
+++ Blogging/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret_key'
+SECRET_KEY = 'django-insecure-@c^lb-a80b_-6tnddn2+q^jsh87k0qzvy_1=40*ln1xun+xl4z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
